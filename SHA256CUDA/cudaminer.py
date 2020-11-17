472fe71b92d88e6a1dc1db7ba0de14913266fc72
vinchua <vchua8@gmail.com>
2020-11-16 22:18:25 -0800

    sleep every 3 minutes, don't do difficulty > 9

--- SHA256CUDA/cudaminer.py
+++ SHA256CUDA/cudaminer.py
@@ -3,6 +3,7 @@ import json
 import base64
 import time
 from subprocess import Popen, PIPE, STDOUT
+from timeit import default_timer as timer
 
 COIN_PREFIX_STR = "CPEN 442 Coin2020"
 LAST_COIN_URL = "http://cpen442coin.ece.ubc.ca/last_coin";
@@ -51,23 +52,22 @@ class Tracker(object):
         print(response)
 
 
-
 def main():
     # echo -n "supersecretcpen442publicid" | sha256sum
     obj = Tracker("534618e186cb7b73677a6b3571fe03c5ef2570d24691e7386ae6ddb62a167c7b")
-
 
     while True:
         list = []
         save_flag = False
 
-        #  nope not doing 10 leading zeros
         current_difficulty = obj.update_difficulty()
-        # if current_difficulty > 9:
-        #     print("Difficulty too high, sleeping for a minute...")
-        #     time.sleep(60)
-        #     continue
+        current_coin = obj.update_last_coin()
+        if current_difficulty > 9:
+            print("Difficulty too high, sleeping for 3 minutes...")
+            time.sleep(180 - time.time() % 180)
+            continue
 
+        timer = time.perf_counter()
         with Popen(["442cointest.exe", obj.last_coin, str(current_difficulty)], stdout=PIPE, stdin=PIPE, stderr=STDOUT) as p:
             for line in p.stdout:
                 line = line.decode('utf-8')
@@ -78,11 +78,18 @@ def main():
                 if "-" in line:
                     save_flag = True
 
+                if time.perf_counter() - timer > 60:
+                    timer = time.perf_counter()
+                    if obj.update_difficulty() != current_difficulty:
+                        print("Difficulty changed. Restarting.")
+                        break;
+                    
         save_flag = False
         check_difficulty = obj.update_difficulty()
         check_last_coin = obj.update_last_coin()
-        if current_difficulty == check_difficulty:
+        if current_difficulty == check_difficulty and current_coin == check_last_coin:
             obj.claim_coin(list)
+        # obj.claim_coin(list)
 
 
 if __name__ == "__main__":
